f21a6a29ac99feea629e79cd1a75d7ed93fbae44
Hoseong-Ryu <ryuhoseong@lyuhoseong-ui-MacBookPro.local>
2025-01-06 15:29:13 +0900

    사용 범위에 맞는 의존성 주입

--- data/src/main/java/com/devhoseong/data/di/RepositoryBindsModule.kt
+++ data/src/main/java/com/devhoseong/data/di/RepositoryBindsModule.kt
@@ -5,10 +5,10 @@ import com.devhoseong.domain.reposiroty.WeatherRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class RepositoryBindsModule {
     @Binds
     abstract fun bindWeatherRepository(
